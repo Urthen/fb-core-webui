08c056811b394c5c113d6f9d0e3b8374c4c8fd89
Michael Pratt <urthen@gmail.com>
2015-04-07 15:28:41 -0500

    add shrinkwrap, clarify keepalive

--- src/keepalive.js
+++ src/keepalive.js
@@ -5,11 +5,11 @@ module.exports = {
     description : 'Makes request to self, keeping the service alive on providers like Heroku.',
 
     init : function (bot) {
-        bot.configLoader.ensure('keepalive_host', 'localhost:3000', 'Host string to ping self at');
-        bot.configLoader.ensure('keepalive_interval', 5, 'Interval in minutes to ping self');
+        bot.configLoader.ensure('web_host', 'localhost:3000', 'Host string of the web front-end');
+        bot.configLoader.ensure('keepalive_interval', 15, 'Interval in minutes to ping self');
 
-        var url = 'http://' + bot.config.keepalive_host + '/health';
-        console.log('Keeping alive every', bot.config.keepalive_interval, 'seconds at', url);
+        var url = 'http://' + bot.config.web_host + '/health';
+        console.log('Keeping alive every', bot.config.keepalive_interval, 'minutes at', url);
         setInterval(function () {
                 request.get(url, function () {});
             }, bot.config.keepalive_interval * 60 * 1000);
