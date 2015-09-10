156b31a5222077b6f0bcee46e5e28ed4c1df6979
Michael Pratt <urthen@gmail.com>
2015-09-09 23:36:20 -0500

    Add sleep interval to keepalive code

--- src/keepalive.js
+++ src/keepalive.js
@@ -5,13 +5,27 @@ module.exports = {
     description : 'Makes request to self, keeping the service alive on providers like Heroku.',
 
     init : function (bot) {
-        bot.configLoader.ensure('web_host', 'localhost:3000', 'Host string of the web front-end');
         bot.configLoader.ensure('keepalive_interval', 15, 'Interval in minutes to ping self');
+        bot.configLoader.ensure('keepalive_sleep', '20:00', 'Time to begin sleep (Heroku needs 6+ hours idle time)');
+        bot.configLoader.ensure('keepalive_wake', '8:00', 'Time to wake up');
 
         var url = 'http://' + bot.config.web_host + '/health';
+        var sleepTimeS = bot.config.keepalive_sleep.split(':');
+        var sleepTime = sleepTimeS[0] * 60 + sleepTimeS[1] * 1;
+        var wakeTimeS = bot.config.keepalive_wake.split(':');
+        var wakeTime = wakeTimeS[0] * 60 + wakeTimeS[1] * 1;
+
         console.log('Keeping alive every', bot.config.keepalive_interval, 'minutes at', url);
         setInterval(function () {
-                request.get(url, function () {});
+                var now = new Date();
+                var nowTime = now.getHours() * 60 + now.getMinutes();
+
+                if (nowTime >= wakeTime && nowTime < sleepTime) {
+                    console.log("Pingedy")
+                    request.get(url, function () {});
+                } else {
+                    console.log("Skipped keepalive due to sleep interval.");
+                }
             }, bot.config.keepalive_interval * 60 * 1000);
     }
 };
--- src/startup.js
+++ src/startup.js
@@ -7,19 +7,12 @@ module.exports = {
 
     init : function (bot) {
 
-        // Heroku needs special attention
         var port = process.env.PORT;
         bot.configLoader.ensure('port', port || '3000', 'Port for web server to listen on');
-
-        // Set up ip whitelisting filter; pass 'undefined' if it's not there
-        // TODO: rip this out when webui user accounts are added
         bot.configLoader.ensure('web_ipfilter', 'undefined', 'IP address(es) for admin panel whitelist');
-
-        // Set theme for web ui
         bot.configLoader.ensure('web_theme', 'united', 'Name of bootswatch.com theme to use for Web-UI');
-
-        // Sessionization secret
         bot.configLoader.ensure('web_secret', 'fritbot', 'Web session secret');
+        bot.configLoader.ensure('web_host', 'localhost:3000', 'Host string of the web front-end');
 
         bot.events.on('modulesLoaded', function () {
             web.startup(bot);
